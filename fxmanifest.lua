f24cfde9d4dfa24d6904ba8d85d333b77e3ccbbe
thegamer5095 <84905478+thegamer5095@users.noreply.github.com>
2024-11-09 17:08:36 +0100

    Main

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_verison 'cerulean'
+
+game 'gta5'
+
+author 'TH'
+
+lua54 'yes'
+
+client_scripts {
+    'client/*.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
+shared_scripts {
+    'config.lua',
+    '@ox_lib/init.lua'
+}
