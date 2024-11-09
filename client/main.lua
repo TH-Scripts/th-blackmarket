7a485339ba699c9a285048df43dcc7dfad2c8a51
huh <itzcbuchbergmc@gmail.com>
2024-11-09 19:22:43 +0100

    Npc and ox target setup

--- client/main.lua
+++ client/main.lua
@@ -1,1 +1,44 @@
--- keep
+-- Local Variables
+local spawnped = false
+
+
+-- NPC Spawn
+--@param Config.NPC.model
+--@param Config.NPC.coords
+
+Citizen.CreateThread(function()
+    while true do
+        if spawnped == false then
+            spawnped = true
+            RequestModel(GetHashKey(Config.NPC.model))
+            while not HasModelLoaded(GetHashKey(Config.NPC.model)) do
+                Wait(1)
+            end
+
+            ped1 = CreatePed(4, GetHashKey(Config.NPC.model), Config.NPC.coords, false, true) -- ændrer disse koordinater
+            FreezeEntityPosition(ped1, true)
+            SetEntityInvincible(ped1, true)
+            SetBlockingOfNonTemporaryEvents(ped1, true)
+        end
+        Citizen.Wait(10000)
+    end
+end)  
+
+
+--OX Target
+--@param Config.Target.coords
+
+exports.ox_target:addSphereZone({
+    coords = Config.Target.coords,
+    radius = 1,
+    debug = drawZones,
+    options = {
+        {
+            icon = 'fa-solid fa-book',
+            label = 'Snak med Jens',
+            onSelect = function()
+                lib.showContext('liste')
+            end
+        }
+    }
+})
---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.NPC = {
+    {
+        coords = vec4(1525.3630, 1710.3300, 109.0087, 352.3030),
+        model = 'a_m_m_prolhost_01',
+    }
+}
+
+Config.Target = {
+    coords = vec3(1525.3630, 1710.3300, 110.0087),
+}
+
